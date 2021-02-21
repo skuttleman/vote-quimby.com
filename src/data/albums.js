036e5b84398e592344665fa646aabd750b11d20e
Ben Allred <skuttleman@gmail.com>
2021-02-21 14:49:50 -0700

    update s3 urls

--- src/data/albums.js
+++ src/data/albums.js
@@ -3,39 +3,39 @@ export default [{
     title: 'Just Can\'t Fuck this Last Pint of Musiclar Famine unless the Death Flight arrives by Next Friday (Theoretically)',
     playList: [{
         title: 'Flight 19',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/01.flight.mp3',
+        url: '/mp3/just-cant-fuck/01.flight.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Last Pint of Happiness',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/02.pint.mp3',
+        url: '/mp3/just-cant-fuck/02.pint.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Fuck Recycling (We Should Play Elevator Music)',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/03.recycling.mp3',
+        url: '/mp3/just-cant-fuck/03.recycling.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Death of a Superhero',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/04.superhero.mp3',
+        url: '/mp3/just-cant-fuck/04.superhero.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Let\'s Go Out Next Friday',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/05.friday.mp3',
+        url: '/mp3/just-cant-fuck/05.friday.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Just Can\'t Please Her',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/06.pleaser.mp3',
+        url: '/mp3/just-cant-fuck/06.pleaser.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Famine 4 (Try the Veal)',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/07.famine4.mp3',
+        url: '/mp3/just-cant-fuck/07.famine4.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Theoretically',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/08.theoretically.mp3',
+        url: '/mp3/just-cant-fuck/08.theoretically.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Musicular Bandslaughter',
-        url: 'https://vote-quimby.s3.amazonaws.com/just-cant-fuck/09.musicular.mp3',
+        url: '/mp3/just-cant-fuck/09.musicular.mp3',
         artist: ['Vote Quimby']
     }]
 },{
@@ -43,23 +43,23 @@ export default [{
     title: 'Talk Nerdy to Me',
     playList: [{
         title: 'Traitors to the Cause',
-        url: 'https://vote-quimby.s3.amazonaws.com/talk-nerdy/01.traitors.mp3',
+        url: '/mp3/talk-nerdy/01.traitors.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Limited Time Only',
-        url: 'https://vote-quimby.s3.amazonaws.com/talk-nerdy/02.limited.mp3',
+        url: '/mp3/talk-nerdy/02.limited.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Sci-Fi Nerd',
-        url: 'https://vote-quimby.s3.amazonaws.com/talk-nerdy/03.sci-fi.mp3',
+        url: '/mp3/talk-nerdy/03.sci-fi.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'Famine 3 (Famished)',
-        url: 'https://vote-quimby.s3.amazonaws.com/talk-nerdy/04.famine3.mp3',
+        url: '/mp3/talk-nerdy/04.famine3.mp3',
         artist: ['Vote Quimby']
     }, {
         title: 'To Be Continued',
-        url: 'https://vote-quimby.s3.amazonaws.com/talk-nerdy/05.tbc.mp3',
+        url: '/mp3/talk-nerdy/05.tbc.mp3',
         artist: ['Vote Quimby']
     }]
 },{
